d15ea27d6771c0f416f783379ae3ef716622041f
jhernandez26 <54816116+jhernandez26@users.noreply.github.com>
2021-04-21 17:00:15 -0500

    Update threads_java.sh
    Se coloca un loop para evitar dependencias de cron

--- threads_java.sh
+++ threads_java.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
-DATA=$(jps)
-rm java_threads.txt 2> /dev/null
-var=''
-while read -r program
+while true
 do
-  PID=$(echo $program | awk '{print $1}')
-  name=$(echo $program | awk '{print $2}')
-  threads=$( jstack $PID  | grep 'java.lang.Thread.State' | wc -l )
-  printf "java_threads{process=\""$name"\", pid=\""$PID"\"} $threads" >> java_threads.txt
-  echo >> java_threads.txt
-done <<< "$DATA"
-server=$(hostname -s)
-curl --data-binary @java_threads.txt http://10.50.166.92:9091/metrics/job/java/monitor/threads/server/$(hostname -s)
+  DATA=$(jps)
+  rm java_threads.txt 2> /dev/null
+  var=''
+  while read -r program
+  do
+    PID=$(echo $program | awk '{print $1}')
+    name=$(echo $program | awk '{print $2}')
+    threads=$( jstack $PID  | grep 'java.lang.Thread.State' | wc -l )
+    printf "java_threads{process=\""$name"\", pid=\""$PID"\"} $threads" >> java_threads.txt
+    echo >> java_threads.txt
+  done <<< "$DATA"
+  server=$(hostname -s)
+  curl --data-binary @java_threads.txt http://10.50.166.92:9091/metrics/job/java/monitor/threads/server/$(hostname -s)
+  sleep 30
+done
+
